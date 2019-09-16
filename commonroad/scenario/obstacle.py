2896acf9c96af38ac7d75582b0a780b2c6979cf3
ga34yeb <markus.koschi@tum.de>
2019-09-16 15:40:23 +0200

    visualization of uncertain states is possible
    - only the center of uncertain intervals for all kind of states is selected for visualization
    - changes for initial states is based on code from Stefanie Manzinger

--- commonroad/scenario/obstacle.py
+++ commonroad/scenario/obstacle.py
@@ -4,7 +4,8 @@ import numpy as np
 from typing import Union
 from abc import ABC, abstractmethod
 
-from commonroad.common.validity import is_valid_orientation, is_real_number_vector, is_real_number
+from commonroad.common.validity import is_valid_orientation, is_real_number_vector, is_real_number, ValidTypes
+from commonroad.common.util import AngleInterval
 from commonroad.geometry.shape import Shape
 from commonroad.prediction.prediction import Prediction, Occupancy
 from commonroad.scenario.trajectory import State
@@ -15,7 +16,7 @@ __credits__ = ["Priority Program SPP 1835 Cooperative Interacting Automobiles, B
 __version__ = "2019.1"
 __maintainer__ = "Stefanie Manzinger"
 __email__ = "commonroad@in.tum.de"
-__status__ = "Released"
+__status__ = "in development"
 
 
 @enum.unique
@@ -225,8 +226,23 @@ class DynamicObstacle(Obstacle):
         occupancy = None
 
         if time_step == self.initial_state.time_step:
-            shape = self.obstacle_shape.rotate_translate_local(
-                self.initial_state.position, self.initial_state.orientation)
+            # ToDo: possibly change such that not only the center of uncertain states is used
+            if isinstance(self.initial_state.position, Shape):
+                position = self.initial_state.position.center
+            elif isinstance(self.initial_state.position, ValidTypes.ARRAY):
+                position = self.initial_state.position
+            else:
+                raise TypeError('<DynamicObstacle/occupancy_at_time> Expected instance of %s or %s. Got '
+                                '%s instead.' % (ValidTypes.ARRAY, Shape, self.initial_state.position.__class__))
+            if isinstance(self.initial_state.orientation, ValidTypes.NUMBERS):
+                orientation = self.initial_state.orientation
+            elif isinstance(self.initial_state.orientation, AngleInterval):
+                orientation = 0.5 * (self.initial_state.orientation.start + self.initial_state.orientation.end)
+            else:
+                raise TypeError('<DynamicObstacle/occupancy_at_time> Expected instance of %s or %s. Got %s '
+                                'instead.' % (ValidTypes.NUMBERS, AngleInterval,
+                                              self.initial_state.orientation.__class__))
+            shape = self.obstacle_shape.rotate_translate_local(position, orientation)
             occupancy = Occupancy(time_step, shape)
         elif time_step > self.initial_state.time_step and self._prediction is not None:
             occupancy = self._prediction.occupancy_at_time_step(time_step)
