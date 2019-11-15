a9188dce76947aa2461ee09ff7e148bc89899b07
Stefanie Manzinger <stefanie.manzinger@in.tum.de>
2019-11-15 10:29:23 +0100

    bugfix script to run all tests

--- commonroad/tests/run_tests.py
+++ commonroad/tests/run_tests.py
@@ -4,14 +4,14 @@ import sys
 
 if __name__ == "__main__":
     print(os.getcwd())
-    errors = 0
+    failures = 0
     tests = 0
     for x in os.walk(os.getcwd()):
         if not '__' in x[0] and not '.' in x[0]:
             print(x[0])
             all_tests = unittest.TestLoader().discover(x[0], pattern='test_*.py')
             b = unittest.TextTestRunner().run(all_tests)
-            errors += len(b.errors)
+            failures += len(b.failures)
             tests += b.testsRun
 
-    print('Executed {} tests and got {} fails'.format(tests,errors))
+    print('Executed {} tests and got {} fails'.format(tests, failures))
