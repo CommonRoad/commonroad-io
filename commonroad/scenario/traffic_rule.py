41688e61b226939f2b8d7b064620943558102ba2
Behtarin Ferdousi <behtarin.ferdousi@tum.de>
2019-12-13 14:14:33 +0100

    Define classes for traffic rules

---
+++ commonroad/scenario/traffic_rule.py
@@ -0,0 +1,102 @@
+__author__ = "Behtarin Ferdousi"
+__copyright__ = "TUM Cyber-Physical Systems Group"
+__credits__ = []
+__version__ = "2019.1"
+__maintainer__ = "Behtarin Ferdousi"
+__email__ = "commonroad@in.tum.de"
+__status__ = "Development"
+
+import enum
+from typing import List
+
+from shapely.geometry import Point
+
+
+@enum.unique
+class TrafficSignID(enum.Enum):
+    MAXSPEED = '274'
+    OVERTAKING = '276'
+    CITYLIMIT = '310'
+    GIVEWAY = '205'
+    STOP = '206'
+    UNKNOWN = ''
+
+
+@enum.unique
+class Direction(enum.Enum):
+    """
+    Enum for all the possible directions for a traffic signal
+    """
+    LEFT = "left"
+    RIGHT = "right"
+    FORWARD = "forward"
+    BACKWARD = "backward"
+
+
+@enum.unique
+class TrafficLightState(enum.Enum):
+    """
+    Enum for the possible types of traffic light in signals
+    """
+    RED = "red"
+    YELLOW = "yellow"
+    RED_YELLOW = "redYellow"
+    GREEN = "green"
+
+
+class TrafficSignElement:
+    """ Class to represent each traffic sign element"""
+    def __init__(self, traffic_sign_id: str,
+                 additional_values: List[int]):
+        self.traffic_sign_id = traffic_sign_id
+        self.additional_values = additional_values
+
+    def __str__(self):
+        return f"Sign Element with id {self.traffic_sign_id} and values {self.additional_values} "
+
+    def __repr__(self):
+        return f"Sign Element with id {self.traffic_sign_id} and values {self.additional_values} "
+
+
+class TrafficSign:
+    """Class to represent traffic sign"""
+    def __init__(self, id: int,
+                 position: Point,
+                 traffic_sign_elements: List[TrafficSignElement],
+                 virtual: bool = False):
+        self.id = id
+        self.position = position
+        self.traffic_sign_elements = traffic_sign_elements
+        self.virtual = virtual
+
+    def __str__(self):
+        return f"Sign At {self.position} with {self.traffic_sign_elements} "
+
+
+class TrafficLightCycleElement:
+    """Class to represent traffic light cycle"""
+    def __init__(self,
+                 state : TrafficLightState,
+                 duration: float):
+        self.state = state
+        self.duration = duration
+
+
+class TrafficLight:
+    """ Class to represent Traffic Light"""
+    def __init__(self, id: int,
+                 cycle: TrafficSignElement,
+                 offset: int = 0,
+                 position: Point = None,
+                 direction: Direction = Direction.FORWARD,
+                 active: bool = True):
+        self.id = id
+        self.cycle = cycle
+        self.offset = offset
+        self.position = position
+        self.direction = direction
+        self.active = active
+
+
+
+
