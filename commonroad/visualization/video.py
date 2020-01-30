2ce23fd6d11e26ba4abf331ade2ed671b91f33a1
Moritz Klischat <moritz.klischat@tum.de>
2020-01-30 17:12:51 +0100

    improved video function

--- commonroad/visualization/video.py
+++ commonroad/visualization/video.py
@@ -3,15 +3,23 @@ from matplotlib.animation import FuncAnimation
 from commonroad.visualization.draw_dispatch_cr import draw_object, plottable_types
 from typing import List, Union
 
-def create_scenario_video(obj: Union[plottable_types, List[plottable_types]], time_begin: int, time_end: int,
-                          file_path: str, plot_limits: Union[list,None], draw_params: Union[dict,None] = None,
-                          fig_size: Union[list,None] = None, fps: int = 10, dpi=80):
+def create_scenario_video(obj_lists: Union[List[plottable_types]], file_path: str, time_begin: int,
+                          time_end: int, delta_time_steps: int=1, plotting_horizon=0, plot_limits: Union[list,None]=None, draw_params: Union[dict,None] = {},
+                          fig_size: Union[list,None] = None, dt=0.1, fps: int = 25, dpi=120):
     """
-    Create scenario video in gif format given the path to the scenario xml
-    :param filename: Name of the video to be saved
-    :param scenario_path: path to the scenario xml used for creating the video gif
-    :param add_only: true if you are only creating new videos and not updating the old ones
-    :return:
+
+    :param obj: list of objects to be plotted. When plotting objects of the same type, use list of lists for speed-up.
+    :param file_path: filename of generated video (ends on .mp4/.gif)
+    :param time_begin: first time step of video
+    :param time_end: last time step of video
+    :param delta_time_steps: plot every delta_time_steps time steps of scenario
+    :param plotting_horizon: time steps of prediction plotted in each frame
+    :param plot_limits: axis limits
+    :param draw_params: draw_params (see draw_object doc)
+    :param fig_size: size of the video
+    :param fps: frames per second
+    :param dpi: resolution of the video
+    :return: None
     """
     assert time_begin < time_end, '<video/create_scenario_video> time_begin=%i needs to smaller than time_end=%i.' % (time_begin,time_end)
 
@@ -26,22 +34,23 @@ def create_scenario_video(obj: Union[plottable_types, List[plottable_types]], ti
         plt.clf()
         plt.gca().set_aspect('equal')
         ax = plt.gca()
-        draw_params.update({'time_begin': time_begin + frame,
-                            'time_end': time_begin + min(frame_count, frame + duration)})
-        draw_object(obj, ax=ax, draw_params=draw_params, plot_limits=plot_limits)
+        draw_params.update({
+            'time_begin': time_begin + delta_time_steps * frame,
+            'time_end': time_begin + min(frame_count,delta_time_steps * frame+plotting_horizon)
+        })
+        for obj in obj_lists:
+            draw_object(obj, ax=ax, draw_params=draw_params, plot_limits=plot_limits)
         # Set limits to assure that each frame has the same size
-
+        ax.autoscale()
         return ln,
 
-    frame_count = time_end - time_begin
+    frame_count = (time_end - time_begin) // delta_time_steps
     # Interval determines the duration of each frame
-    interval = 1.0/fps
-
-    # length of trajecotry steps
-    duration = 1
-
+    interval = 1.0/dt
+    plt.ioff()
     anim = FuncAnimation(fig, update, frames=frame_count,
-                         init_func=update, blit=True, interval=interval)
+                         init_func=update, blit=False, interval=interval)
     anim.save(file_path, dpi=dpi,
               writer='imagemagick')
+
     plt.close(plt.gcf())
