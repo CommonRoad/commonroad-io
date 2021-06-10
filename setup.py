c27db3695272a1d1c3241e868b95f9a56534952e
Moritz Klischat <moritz.klischat@tum.de>
2021-06-10 10:11:01 +0200

    add create_dynamic_obstacle to Solution

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(
         'networkx>=2.2',
         'Pillow>=7.0.0',
         'iso3166>=1.0.1',
+        'commonroad-vehicle-models>=2.0.0'
     ],
     extras_require={
         'doc': ['sphinx>=1.3.6',
