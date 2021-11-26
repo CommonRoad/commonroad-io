a6efbfe960f59cf92278251fe2f7b90f17e42690
ge64gux <yingye.xin@tum.de>
2021-11-26 14:39:32 +0100

    change for style check

--- commonroad/tests/common/test_solution.py
+++ commonroad/tests/common/test_solution.py
@@ -802,8 +802,10 @@ class TestCommonRoadSolutionReader(unittest.TestCase):
 
         assert str(parsed_solution_single.scenario_id) == str(self.solution_single.scenario_id)
         assert str(parsed_solution_collab.scenario_id) == str(self.solution_collab.scenario_id)
-        assert parsed_solution_single.date.strftime('%Y-%m-%dT%H:%M:%S') == self.solution_single.date.strftime('%Y-%m-%dT%H:%M:%S')
-        assert parsed_solution_collab.date.strftime('%Y-%m-%dT%H:%M:%S') == self.solution_collab.date.strftime('%Y-%m-%dT%H:%M:%S')
+        assert parsed_solution_single.date.strftime('%Y-%m-%dT%H:%M:%S') == 
+                                self.solution_single.date.strftime('%Y-%m-%dT%H:%M:%S')
+        assert parsed_solution_collab.date.strftime('%Y-%m-%dT%H:%M:%S') == 
+                                self.solution_collab.date.strftime('%Y-%m-%dT%H:%M:%S')
         assert parsed_solution_single.computation_time == self.solution_single.computation_time
         assert parsed_solution_collab.computation_time == self.solution_collab.computation_time
         assert parsed_solution_single.processor_name == self.solution_single.processor_name
@@ -842,8 +844,10 @@ class TestCommonRoadSolutionReader(unittest.TestCase):
 
         assert str(parsed_solution_single.scenario_id) == str(self.solution_single.scenario_id)
         assert str(parsed_solution_collab.scenario_id) == str(self.solution_collab.scenario_id)
-        assert parsed_solution_single.date.strftime('%Y-%m-%dT%H:%M:%S') == self.solution_single.date.strftime('%Y-%m-%dT%H:%M:%S')
-        assert parsed_solution_collab.date.strftime('%Y-%m-%dT%H:%M:%S') == self.solution_collab.date.strftime('%Y-%m-%dT%H:%M:%S')
+        assert parsed_solution_single.date.strftime('%Y-%m-%dT%H:%M:%S') == 
+                                self.solution_single.date.strftime('%Y-%m-%dT%H:%M:%S')
+        assert parsed_solution_collab.date.strftime('%Y-%m-%dT%H:%M:%S') == 
+                                self.solution_collab.date.strftime('%Y-%m-%dT%H:%M:%S')
         assert parsed_solution_single.computation_time == self.solution_single.computation_time
         assert parsed_solution_collab.computation_time == self.solution_collab.computation_time
         assert parsed_solution_single.processor_name == self.solution_single.processor_name
@@ -867,8 +871,10 @@ class TestCommonRoadSolutionReader(unittest.TestCase):
 
         assert str(parsed_solution_single.scenario_id) == str(self.solution_single.scenario_id)
         assert str(parsed_solution_collab.scenario_id) == str(self.solution_collab.scenario_id)
-        assert parsed_solution_single.date.strftime('%Y-%m-%dT%H:%M:%S') == self.solution_single.date.strftime('%Y-%m-%dT%H:%M:%S')
-        assert parsed_solution_collab.date.strftime('%Y-%m-%dT%H:%M:%S') == self.solution_collab.date.strftime('%Y-%m-%dT%H:%M:%S')
+        assert parsed_solution_single.date.strftime('%Y-%m-%dT%H:%M:%S') == 
+                                self.solution_single.date.strftime('%Y-%m-%dT%H:%M:%S')
+        assert parsed_solution_collab.date.strftime('%Y-%m-%dT%H:%M:%S') == 
+                                self.solution_collab.date.strftime('%Y-%m-%dT%H:%M:%S')
         assert parsed_solution_single.computation_time == self.solution_single.computation_time
         assert parsed_solution_collab.computation_time == self.solution_collab.computation_time
         assert parsed_solution_single.processor_name == self.solution_single.processor_name
