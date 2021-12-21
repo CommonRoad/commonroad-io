3addfbd0f0ae9fa77aa3f8175ba249322dc8cc8e
Sebastian Maierhofer <sebastian.maierhofer@tum.de>
2021-12-21 09:35:25 +0100

    reformat code

--- setup.py
+++ setup.py
@@ -8,18 +8,17 @@ with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
     readme = f.read()
 
 setup(name='commonroad-io', version='2021.4',
-        description='Python tool to read, write, and visualize CommonRoad scenarios and solutions for automated '
-                    'vehicles.',
-        keywords='autonomous automated vehicles driving motion planning', url='https://commonroad.in.tum.de/',
-        project_urls={'Documentation': 'https://commonroad.in.tum.de/static/docs/commonroad-io/index.html',
-            'Forum': 'https://commonroad.in.tum.de/forum/c/commonroad-io',
-            'Source': 'https://gitlab.lrz.de/tum-cps/commonroad_io', },
-        author='Cyber-Physical Systems Group, Technical University of Munich', author_email='commonroad@lists.lrz.de',
-        license="BSD", packages=find_packages(exclude=['doc', 'tests', 'tutorials']),
-        install_requires=['numpy>=1.13', 'scipy>=1.5.2', 'shapely>=1.6.4', 'matplotlib>=3.0.0', 'lxml>=4.2.2',
-            'networkx>=2.2', 'Pillow>=7.0.0', 'iso3166>=1.0.1',
-        'commonroad-vehicle-models>=2.0.0',
-        'rtree>=0.8.3',
+      description='Python tool to read, write, and visualize CommonRoad scenarios and solutions for automated '
+                  'vehicles.',
+      keywords='autonomous automated vehicles driving motion planning', url='https://commonroad.in.tum.de/',
+      project_urls={'Documentation': 'https://commonroad.in.tum.de/static/docs/commonroad-io/index.html',
+                    'Forum': 'https://commonroad.in.tum.de/forum/c/commonroad-io',
+                    'Source': 'https://gitlab.lrz.de/tum-cps/commonroad_io', },
+      author='Cyber-Physical Systems Group, Technical University of Munich', author_email='commonroad@lists.lrz.de',
+      license="BSD", packages=find_packages(exclude=['doc', 'tests', 'tutorials']),
+      install_requires=['numpy>=1.13', 'scipy>=1.5.2', 'shapely>=1.6.4', 'matplotlib>=3.0.0', 'lxml>=4.2.2',
+                        'networkx>=2.2', 'Pillow>=7.0.0', 'iso3166>=1.0.1', 'commonroad-vehicle-models>=2.0.0',
+                        'rtree>=0.8.3',
     ],
     extras_require={
         'doc': ['sphinx>=1.3.6',
