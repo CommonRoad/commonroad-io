3af8a228ddc33d0860f5f61ca319b5e061cc84ca
Sebastian Maierhofer <sebastian.maierhofer@tum.de>
2022-04-04 07:25:23 +0200

    reformat code

--- commonroad/__init__.py
+++ commonroad/__init__.py
@@ -1,4 +1,4 @@
 import numpy
-SCENARIO_VERSION = '3.0' # new default scenario version for writing scenario files
+SCENARIO_VERSION = '3.0'
 SUPPORTED_COMMONROAD_VERSIONS = {'2018b', '2020a', '3.0'}  # supported version for reading scenario files
 TWO_PI = 2.0 * numpy.pi
