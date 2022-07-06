d2b904ae9e1a0c85402c3ac7040179ed084966f1
Nachiket497 <1000nachiket@gmail.com>
2022-07-06 21:11:29 +0530

    reformated code_example.py & write_trajectory.py

--- tutorials/code_example.py
+++ tutorials/code_example.py
@@ -9,6 +9,7 @@ from commonroad.scenario.trajectory import State
 from commonroad.scenario.obstacle import DynamicObstacle
 from commonroad.scenario.trajectory import Trajectory
 from commonroad.prediction.prediction import TrajectoryPrediction
+
 # generate path of the file to be opened
 file_path = "ZAM_Tutorial-1_1_T-1.xml"
 
@@ -18,18 +19,16 @@ scenario, planning_problem_set = CommonRoadFileReader(file_path).open()
 # generate the static obstacle according to the specification, refer to API for details of input parameters
 static_obstacle_id = scenario.generate_object_id()
 static_obstacle_type = ObstacleType.PARKED_VEHICLE
-static_obstacle_shape = Rectangle(width = 2.0, length = 4.5)
-static_obstacle_initial_state = State(position = np.array([30.0, 3.5]), orientation = 0.02, time_step = 0)
+static_obstacle_shape = Rectangle(width=2.0, length=4.5)
+static_obstacle_initial_state = State(position=np.array([30.0, 3.5]), orientation=0.02, time_step=0)
 
-static_obstacle = StaticObstacle(static_obstacle_id, static_obstacle_type, static_obstacle_shape, static_obstacle_initial_state)
+static_obstacle = StaticObstacle(static_obstacle_id, static_obstacle_type, static_obstacle_shape,
+                                 static_obstacle_initial_state)
 
 scenario.add_objects(static_obstacle)
 
 # initial state has a time step of 0
-dynamic_obstacle_initial_state = State(position = np.array([50.0, 0.0]),
-                                       velocity = 22,
-                                       orientation = 0.02,
-                                       time_step = 0)
+dynamic_obstacle_initial_state = State(position=np.array([50.0, 0.0]), velocity=22, orientation=0.02, time_step=0)
 
 # generate the states for the obstacle for time steps 1 to 40 by assuming constant velocity
 state_list = []
@@ -37,7 +36,7 @@ for i in range(1, 100):
     # compute new position
     new_position = np.array([dynamic_obstacle_initial_state.position[0] + scenario.dt * i * 22, scenario.dt * i])
     # create new state
-    new_state = State(position = new_position, velocity = 22,orientation = 1/22, time_step = i, hitch = -1/44)
+    new_state = State(position=new_position, velocity=22, orientation=1 / 22, time_step=i, hitch=-1 / 44)
     # add new state to state_list
     state_list.append(new_state)
 
@@ -46,16 +45,14 @@ dynamic_obstacle_trajectory = Trajectory(1, state_list)
 
 # create the prediction using the trajectory and the shape of the obstacle
 dynamic_obstacle_shape = ShapeGroup([Rectangle(5.1, 2.55), Rectangle(13.6, 2.55)])
-dynamic_obstacle_prediction = TrajectoryPrediction(dynamic_obstacle_trajectory, dynamic_obstacle_shape, wheelbase_lengths=[3.6, 8.1])
+dynamic_obstacle_prediction = TrajectoryPrediction(dynamic_obstacle_trajectory, dynamic_obstacle_shape,
+                                                   wheelbase_lengths=[3.6, 8.1])
 
 # generate the dynamic obstacle according to the specification
 dynamic_obstacle_id = scenario.generate_object_id()
 dynamic_obstacle_type = ObstacleType.TRUCK
-dynamic_obstacle = DynamicObstacle(dynamic_obstacle_id,
-                                   dynamic_obstacle_type,
-                                   dynamic_obstacle_shape,
-                                   dynamic_obstacle_initial_state,
-                                   dynamic_obstacle_prediction)
+dynamic_obstacle = DynamicObstacle(dynamic_obstacle_id, dynamic_obstacle_type, dynamic_obstacle_shape,
+                                   dynamic_obstacle_initial_state, dynamic_obstacle_prediction)
 
 # add dynamic obstacle to the scenario
 scenario.add_objects(dynamic_obstacle)
@@ -69,17 +66,3 @@ for i in range(5):
     plt.show()
 
 fig = plt.figure(figsize=(25, 10))
-
-# def animation_frame(i):
-#     # zoom in the animation (plot_limits can be removed if not needed)
-#     rnd = MPRenderer()
-#     # plot the scenario at different time step
-#     scenario.draw(rnd, draw_params={'time_begin': i, 'time_end': 1573})
-#     return rnd.render()
-
-# from matplotlib.animation import FuncAnimation
-
-
-# # animate
-# animation = FuncAnimation(fig, func=animation_frame, frames=range(0, 1573, 5), interval=0.05)
-# plt.show()
--- tutorials/write_trajectory.py
+++ tutorials/write_trajectory.py
@@ -9,6 +9,7 @@ def read_json_file(file_path):
     f.close()
     return data
 
+
 def get_local_coords(x_corr, y_corr):
     local_x = []
     local_y = []
@@ -17,9 +18,10 @@ def get_local_coords(x_corr, y_corr):
         local_y.append(y_corr[i] - OFFSET_Y)
     return local_x, local_y
 
-# update coordinates with the corrected central position for truck
-# calculated from the real wheel position, angle and wheelbase length
+
 def update_coords(solution):
+    """  update coordinates with the corrected central position for truck
+         calculated from the real wheel position, angle and wheelbase length"""
     s_x, s_y = solution['x'], solution['y']
     theta = solution['theta']
     x_updated, y_updated = [], []
@@ -28,39 +30,34 @@ def update_coords(solution):
         y_updated.append(s_y[i] + 0.5 * np.sin(theta[i]) * 3.6)
     return x_updated, y_updated
 
+
 def get_trajectory(json_data):
     solution = json_data['final_solution']
     # update coordinates to the CommonRoad model
     x_corr, y_corr = update_coords(solution)
     hitch_angles = solution['alpha']
     v_long = solution['direction']
-    steering_angles = solution['phi'] # delta
+    steering_angles = solution['phi']  # delta
     orientation = solution['theta']
     # calculate CommonRoad coordinates from world coordinates
     s_x, s_y = get_local_coords(x_corr, y_corr)
     return hitch_angles, v_long, steering_angles, orientation, s_x, s_y
 
-# write trajectory entry
+
 def write_commonroad_trajectory(json_data):
+    """write trajectory entry"""
     hitch, v_long, steering, orient, x, y = get_trajectory(json_data)
     states = []
     for i in range(len(x)):
         s_x, s_y, ori = str(x[i]), str(y[i]), str(orient[i])
         time, vel, hit = str(i + 1), str(v_long[i]), str(hitch[i])
-        state = State(
-            position=np.array([x[i], y[i]]),
-            velocity=v_long[i],
-            orientation=orient[i],
-            steering_angle=steering[i],
-            time_step=i,
-            hitch=hitch[i]
-        )
-        # state = [np.array([x[i], y[i]]),v_long[i],orient[i],steering[i],i,hitch[i] ]
+        state = State(position=np.array([x[i], y[i]]), velocity=v_long[i], orientation=orient[i],
+                      steering_angle=steering[i], time_step=i, hitch=hitch[i])
         states.append(state)
     return states
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     TRAJECTORY_FILE = "04M001.json"
     # offset between world and local (CommonRoad) coordinates
     OFFSET_X = 561676.6763867161
@@ -68,18 +65,7 @@ if __name__ == "__main__":
     # read trajectory data
     trajectory_data = read_json_file(TRAJECTORY_FILE)
     states = write_commonroad_trajectory(trajectory_data)
-    data = np.array( states, dtype=object)
-    output_npz_file = 'trajectory'
-    np.save(output_npz_file, data)
-
-    # to read file
-    # np_load_old = np.load
-
-    # # # modify the default parameters of np.load
-    # np.load = lambda *a,**k: np_load_old(*a, allow_pickle=True, **k)
-    # st = np.load(output_npz_file+".npy")
-    # # st = [ State(position=st[i][0], velocity=st[i][1], orientation=st[i][2], steering_angle=st[i][3], time_step=st[i][4], hitch=st[i][5]) for i in range(len(st))]
-
-    # np.load = np_load_old
-    # print(st[0])
-
+    data = np.array(states, dtype=object)
+    output_npy_file = 'trajectory'
+    np.save(output_npy_file, data)
+    print("Trajectory saved to {}.npy".format(output_npz_file))
