df2c9530506528d468d849f36086e62018ad9a79
Sebastian Maierhofer <sebastian.maierhofer@tum.de>
2023-06-29 07:54:13 +0200

    update protobuf generation script to create correct import paths

--- scenario_definition/protobuf_format/generate_pb.sh
+++ scenario_definition/protobuf_format/generate_pb.sh
@@ -2,27 +2,24 @@
 
 # generate map
 protoc \
---proto_path=./definition_files/common/ \
---proto_path=./definition_files/map/ \
---python_out=../../commonroad/common/pb_scripts/map/ \
-./definition_files/map/*.proto
+--proto_path=./ \
+--python_out=../../ \
+./commonroad/common/pb_scripts/map/*.proto
 
 # generate dynamic
 protoc \
---proto_path=./definition_files/common/ \
---proto_path=./definition_files/dynamic/ \
---python_out=../../commonroad/common/pb_scripts/dynamic/ \
-./definition_files/dynamic/*.proto
+--proto_path=./ \
+--python_out=../../ \
+./commonroad/common/pb_scripts/dynamic/*.proto
 
-## generate scenario
+### generate scenario
 protoc \
---proto_path=./definition_files/common/ \
---proto_path=./definition_files/scenario/ \
---python_out=../../commonroad/common/pb_scripts/scenario/ \
-./definition_files/scenario/*.proto
+--proto_path=./ \
+--python_out=../../ \
+./commonroad/common/pb_scripts/scenario/*.proto
 
 ## generate rest
 protoc \
---proto_path=./definition_files/common/ \
---python_out=../../commonroad/common/pb_scripts/common/ \
-./definition_files/common/*.proto
+--proto_path=./ \
+--python_out=../../ \
+./commonroad/common/pb_scripts/common/*.proto
