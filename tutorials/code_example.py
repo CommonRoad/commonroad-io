f5afeddd50d553a31cc514edc0827bf5979c7316
Sebastian Maierhofer <sebastian.maierhofer@tum.de>
2023-07-03 09:35:51 +0200

    update example script

--- tutorials/code_example.py
+++ tutorials/code_example.py
@@ -1,26 +1,19 @@
 # import functions to read xml file and visualize commonroad objects
 import matplotlib.pyplot as plt
-
-from commonroad.common.file_reader import CommonRoadFileReader, CommonRoadReadAll, CommonRoadMapFileReader, CommonRoadDynamicFileReader, CommonRoadScenarioFileReader
-from commonroad.common.file_writer import CommonRoadFileWriter, FileFormat, OverwriteExistingFile
+from commonroad.common.file_reader import CommonRoadReadAll
+from commonroad.visualization.mp_renderer import MPRenderer
 
 # generate path of the file to be opened
-file_path_xml = "ZAM_Tutorial-1_1_T-1.xml"
-file_path_map = "ZAM_Tutorial-1.pb"
-file_path_dy = "ZAM_Tutorial-1_1_T-1.pb"
-file_path_sc = "ZAM_Tutorial-1_1_T-1-SC.pb"
+file_path = "ZAM_Tutorial-1_1_T-1.pb"
 
 # read in the scenario and planning problem set
-sc, pb = CommonRoadFileReader(file_path_xml).open()
+scenario, planning_problem_set, _ = CommonRoadReadAll(file_path).open()
 
-fw = CommonRoadFileWriter(sc, pb)
-fw.write_map_to_file(overwrite_existing_file=OverwriteExistingFile.ALWAYS)
-fw.write_scenario_to_file(overwrite_existing_file=OverwriteExistingFile.ALWAYS)
-fw.write_dynamic_to_file(overwrite_existing_file=OverwriteExistingFile.ALWAYS)
-
-CommonRoadMapFileReader(file_path_map).open()
-CommonRoadScenarioFileReader(file_path_sc).open()
-CommonRoadDynamicFileReader(file_path_dy).open()
-
-CommonRoadReadAll(file_path_dy).open()
-CommonRoadReadAll(file_path_sc).open()
+# plot the planning problem and the scenario for the fifth time step
+plt.figure(figsize=(25, 10))
+rnd = MPRenderer()
+rnd.draw_params.time_begin = 5
+scenario.draw(rnd)
+planning_problem_set.draw(rnd)
+rnd.render()
+plt.show()
