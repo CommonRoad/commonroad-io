86c551579dd3f5fea9d8e971039de48b9dd20db2
Sebastian Maierhofer <sebastian.maierhofer@tum.de>
2023-12-29 15:24:19 +0100

    update pb format

--- commonroad/common/protobuf/common/scenario_meta_information_pb2.py
+++ commonroad/common/protobuf/common/scenario_meta_information_pb2.py
@@ -20,7 +20,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   syntax='proto2',
   serialized_options=None,
   create_key=_descriptor._internal_create_key,
-  serialized_pb=b'\n\x1fscenario_meta_information.proto\x12\x11\x63ommonroad_common\x1a\nutil.proto\"=\n\x05MapID\x12\x12\n\ncountry_id\x18\x01 \x02(\t\x12\x10\n\x08map_name\x18\x02 \x02(\t\x12\x0e\n\x06map_id\x18\x03 \x02(\x04\"\xb1\x01\n\nScenarioID\x12\x13\n\x0b\x63ooperative\x18\x01 \x02(\x08\x12(\n\x06map_id\x18\x02 \x02(\x0b\x32\x18.commonroad_common.MapID\x12\x18\n\x10\x63onfiguration_id\x18\x05 \x02(\x04\x12\x19\n\x11obstacle_behavior\x18\x06 \x01(\t\x12\x15\n\rprediction_id\x18\x07 \x01(\x04\x12\x18\n\x10scenario_version\x18\x08 \x02(\t\"\x9e\x01\n\x0f\x46ileInformation\x12\x14\n\x0clicense_name\x18\x01 \x02(\t\x12\x14\n\x0clicense_text\x18\x02 \x01(\t\x12*\n\x04\x64\x61te\x18\x03 \x02(\x0b\x32\x1c.commonroad_common.TimeStamp\x12\x0e\n\x06\x61uthor\x18\x04 \x02(\t\x12\x13\n\x0b\x61\x66\x66iliation\x18\x05 \x02(\t\x12\x0e\n\x06source\x18\x06 \x02(\t\"\xa4\x01\n\x17ScenarioMetaInformation\x12\x33\n\x0c\x62\x65nchmark_id\x18\x02 \x02(\x0b\x32\x1d.commonroad_common.ScenarioID\x12<\n\x10\x66ile_information\x18\x03 \x02(\x0b\x32\".commonroad_common.FileInformation\x12\x16\n\x0etime_step_size\x18\x04 \x02(\x01'
+  serialized_pb=b'\n\x1fscenario_meta_information.proto\x12\x11\x63ommonroad_common\x1a\nutil.proto\"=\n\x05MapID\x12\x12\n\ncountry_id\x18\x01 \x02(\t\x12\x10\n\x08map_name\x18\x02 \x02(\t\x12\x0e\n\x06map_id\x18\x03 \x02(\x04\"\xb1\x01\n\nScenarioID\x12\x13\n\x0b\x63ooperative\x18\x01 \x02(\x08\x12(\n\x06map_id\x18\x02 \x02(\x0b\x32\x18.commonroad_common.MapID\x12\x18\n\x10\x63onfiguration_id\x18\x03 \x02(\x04\x12\x19\n\x11obstacle_behavior\x18\x04 \x01(\t\x12\x15\n\rprediction_id\x18\x05 \x01(\x04\x12\x18\n\x10scenario_version\x18\x06 \x02(\t\"\x9e\x01\n\x0f\x46ileInformation\x12\x14\n\x0clicense_name\x18\x01 \x02(\t\x12\x14\n\x0clicense_text\x18\x02 \x01(\t\x12*\n\x04\x64\x61te\x18\x03 \x02(\x0b\x32\x1c.commonroad_common.TimeStamp\x12\x0e\n\x06\x61uthor\x18\x04 \x02(\t\x12\x13\n\x0b\x61\x66\x66iliation\x18\x05 \x02(\t\x12\x0e\n\x06source\x18\x06 \x02(\t\"\xa4\x01\n\x17ScenarioMetaInformation\x12\x33\n\x0c\x62\x65nchmark_id\x18\x01 \x02(\x0b\x32\x1d.commonroad_common.ScenarioID\x12<\n\x10\x66ile_information\x18\x02 \x02(\x0b\x32\".commonroad_common.FileInformation\x12\x16\n\x0etime_step_size\x18\x03 \x02(\x01'
   ,
   dependencies=[util__pb2.DESCRIPTOR,])
 
@@ -97,28 +97,28 @@ _SCENARIOID = _descriptor.Descriptor(
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
     _descriptor.FieldDescriptor(
       name='configuration_id', full_name='commonroad_common.ScenarioID.configuration_id', index=2,
-      number=5, type=4, cpp_type=4, label=2,
+      number=3, type=4, cpp_type=4, label=2,
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
     _descriptor.FieldDescriptor(
       name='obstacle_behavior', full_name='commonroad_common.ScenarioID.obstacle_behavior', index=3,
-      number=6, type=9, cpp_type=9, label=1,
+      number=4, type=9, cpp_type=9, label=1,
       has_default_value=False, default_value=b"".decode('utf-8'),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
     _descriptor.FieldDescriptor(
       name='prediction_id', full_name='commonroad_common.ScenarioID.prediction_id', index=4,
-      number=7, type=4, cpp_type=4, label=1,
+      number=5, type=4, cpp_type=4, label=1,
       has_default_value=False, default_value=0,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
     _descriptor.FieldDescriptor(
       name='scenario_version', full_name='commonroad_common.ScenarioID.scenario_version', index=5,
-      number=8, type=9, cpp_type=9, label=2,
+      number=6, type=9, cpp_type=9, label=2,
       has_default_value=False, default_value=b"".decode('utf-8'),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
@@ -217,21 +217,21 @@ _SCENARIOMETAINFORMATION = _descriptor.Descriptor(
   fields=[
     _descriptor.FieldDescriptor(
       name='benchmark_id', full_name='commonroad_common.ScenarioMetaInformation.benchmark_id', index=0,
-      number=2, type=11, cpp_type=10, label=2,
+      number=1, type=11, cpp_type=10, label=2,
       has_default_value=False, default_value=None,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
     _descriptor.FieldDescriptor(
       name='file_information', full_name='commonroad_common.ScenarioMetaInformation.file_information', index=1,
-      number=3, type=11, cpp_type=10, label=2,
+      number=2, type=11, cpp_type=10, label=2,
       has_default_value=False, default_value=None,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
     _descriptor.FieldDescriptor(
       name='time_step_size', full_name='commonroad_common.ScenarioMetaInformation.time_step_size', index=2,
-      number=4, type=1, cpp_type=5, label=2,
+      number=3, type=1, cpp_type=5, label=2,
       has_default_value=False, default_value=float(0),
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
