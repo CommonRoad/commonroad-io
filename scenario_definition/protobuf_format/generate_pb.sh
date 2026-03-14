19c4f94627f0236a1357561a4496a24882705d22
andrew <j.andregon15@gmail.com>
2026-03-14 01:13:35 +0100

    Style errors

--- scenario_definition/protobuf_format/generate_pb.sh
+++ scenario_definition/protobuf_format/generate_pb.sh
@@ -1,28 +1,28 @@
-#! /bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
 
-# generate map
-protoc \
---proto_path=./common/ \
---proto_path=./map/ \
---python_out=../../commonroad/common/protobuf/map/ \
-./map/*.proto
+PROTO_ROOT=.
+OUT=../../commonroad/common/protobuf
 
-# generate dynamic
-protoc \
---proto_path=./common/ \
---proto_path=./dynamic/ \
---python_out=../../commonroad/common/protobuf/dynamic/ \
-./dynamic/*.proto
+rm -rf "$OUT"/common "$OUT"/map "$OUT"/dynamic "$OUT"/scenario
+mkdir -p "$OUT"/{common,map,dynamic,scenario}
 
-## generate scenario
-protoc \
---proto_path=./common/ \
---proto_path=./scenario/ \
---python_out=../../commonroad/common/protobuf/scenario/ \
-./scenario/*.proto
+protoc -I "$PROTO_ROOT" --python_out="$OUT" \
+  common/*.proto map/*.proto dynamic/*.proto scenario/*.proto
 
-## generate rest
-protoc \
---proto_path=./common/ \
---python_out=../../commonroad/common/protobuf/common/ \
-./common/*.proto
+touch "$OUT/__init__.py"
+touch "$OUT/common/__init__.py"
+touch "$OUT/map/__init__.py"
+touch "$OUT/dynamic/__init__.py"
+touch "$OUT/scenario/__init__.py"
+
+PY_PKG=commonroad.common.protobuf
+
+find "$OUT" -name '*_pb2.py' -type f | while read -r f; do
+  sed -i \
+    -e "s/^from common import /from ${PY_PKG}.common import /" \
+    -e "s/^from map import /from ${PY_PKG}.map import /" \
+    -e "s/^from dynamic import /from ${PY_PKG}.dynamic import /" \
+    -e "s/^from scenario import /from ${PY_PKG}.scenario import /" \
+    "$f"
+done
